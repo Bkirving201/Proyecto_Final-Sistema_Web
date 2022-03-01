2279f2a4cc9ff3446c480661668e13725898a90a
Bkirving201 <86727411+Bkirving201@users.noreply.github.com>
2022-03-01 15:11:28 -0600

    Entrega 4 : Base de datos
    [ Cambios ]
    
    - Se han cambiado los nombres de los archivos para la referencia entre documentos
    - Se agrego la conexion del sistema a bases de datos por medio de Xampp y PHPmyadmid
    - Se agrego su previo documento para poder mostrar mensaje de conectividad
    - Modificación al codigo fuente en base a correcciones de sintaxis

---
+++ Proyecto-web/Index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="styles.css"> <!-- Conexion HTML con CSS -->
+	<link rel="preconnect" href="https://fonts.googleapis.com"> <!-- google fonts -->
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!-- google fonts -->
+	<link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet"> <!-- titulo fuente -->
+	<link href="https://fonts.googleapis.com/css2?family=Gideon+Roman&display=swap" rel="stylesheet"> <!-- fuente general-->
+	<title>Sistema hotelero (beta 1)</title>
+</head>
+<body>
+	<div id="general">
+
+		<!-- aqui comienza la Barra de navegacion lateral -->
+		<div id="navegacion">
+
+			<div id="navegacion-alineacion">
+
+				<img src="img2.png" id="img1">  <!-- logo -->
+			<a href="Index.php">
+				<button class="animacion-btn" id="boton">Inicio</button>
+			</a>
+
+			<a href="Registro.php">
+				<button class="animacion-btn" id="boton">Registro</button> <!-- Btn-lateral-Regristo -->
+			</a>
+			<a href="Habitaciones.php">
+				<button class="animacion-btn" id="boton">Habitaciones</button> <!-- Btn-lateral-Habitaciones -->
+			</a>
+				<button class="animacion-btn" id="boton">Datos registrados</button> <!-- Btn-lateral-Regristo -->
+			</div>
+		</div>
+		<!-- aqui termina la Barra de navegacion lateral -->
+		
+		<div id="cuerpo">
+			<div id="contenido-cuerpo"> <!--Cuadro branco -->
+
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
